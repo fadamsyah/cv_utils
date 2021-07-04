db99a26de077e83873fdb28f762341a069d1b867
adamsyah <fadillahadam11@gmail.com>
2021-07-04 13:49:03 +0700

    Add yolo_to_coco function

--- cv_utils/object_detection/dataset/converter.py
+++ cv_utils/object_detection/dataset/converter.py
@@ -12,6 +12,8 @@ def coco_to_yolo(coco_annotation_path, coco_image_folder,
                  output_folder, output_set_name):
     """COCO --> YOLO
     
+    This code uses the ultralytics/yolov5 format.
+    The converted dataset will be saved as follow:
     - {output_folder}
         - images
             - {output_set_name}
@@ -97,3 +99,92 @@ def coco_to_yolo(coco_annotation_path, coco_image_folder,
     print("[INFO] names: {}".format(categories))
     print('-' * 75)
     print('')
+    
+def yolo_to_coco(yolo_image_dir,  yolo_label_dir, yoyo_class_file,
+                 coco_image_dir, coco_annotation_path):
+    
+    # Create the image directory
+    Path(coco_image_dir).mkdir(parents=True, exist_ok=True)
+    for Path in os.listdir(coco_image_dir):
+        try: os.remove(os.path.join(coco_image_dir, path))
+        except IsADirectoryError: shutil.rmtree(os.path.join(coco_image_dir, path))
+    
+    # Create the annotation directory (if any)
+    coco_annotation_dirname = os.path.dirname(coco_annotation_path)
+    if len(coco_annotation_dirname) != 0:
+        Path(coco_annotation_dirname).mkdir(parents=True, exist_ok=True)
+        
+    # Get dataset class from .txt file
+    with open(yoyo_class_file, "r") as f:
+        yolo_class = f.read().splitlines()
+    
+    # Format dataset class into categories as in COCO
+    # COCO idx starts from 1 whereas YOLO idx starts from 0
+    coco_annotation = {
+        "type": "instances",
+        "categories": [
+            {
+                "supercategory": "none",
+                "name": class_name,
+                "id": class_id + 1
+            }
+            for class_id, class_name in enumerate(yoyo_class)
+        ],
+        "images": [],
+        "annotations": []
+    }
+    
+    # Loop over all images inside the yolo_image_dir
+    image_id = 1
+    annotation_id = 1
+    for image_name in os.listdir(yolo_image_dir):
+        # Copy the image
+        shutil.copy2(os.path.join(yolo_image_dir, image_name),
+                     os.path.join(coco_image_dir, image_name))
+        
+        # Get the baseline of image name (without extension)
+        name = os.path.splitext(image_name)
+        
+        # Add the image into coco_annotation
+        width, height = PIL.Image.open(
+            os.path.join(coco_image_dir, image_name)
+        ).size
+        coco_annotation["images"].append(
+            {
+                "file_name": image_name,
+                "height": height,
+                "width": width,
+                "id": image_id
+            }
+        )
+        
+        # Add annotations of the image into coco_annotation
+        with open(os.path.join(yolo_label_dir, name)+'.txt', "r") as f:
+            annotations = f.read().splitlines()
+        for annotation in annotations:
+            cat_id, x, y, w, h = annotation.split(' ')
+            
+            xmin = max(float(x) - float(w)/2., 0.) * width
+            ymin = max(float(y) - float(h)/2., 0.) * height
+            xmax = min(float(x) + float(w)/2., 1.) * width
+            ymax = min(float(y) + float(h)/2., 1.) * height
+            dx = xmax - xmin
+            dy = ymax - ymin
+            
+            coco_annotation["annotations"].append(
+                "id": annotation_id,
+                "bbox": [
+                    xmin, ymin, dx, dy
+                ],
+                "image_id": image_id,
+                "category_id": cat_id + 1,
+                "segmentation": [],
+                "area": dx * dy,
+                "iscrowd": 0
+            )
+            
+            # Update the annotation_id
+            annotation_id += 1
+        
+        # Update the image_id
+        image_id += 1
