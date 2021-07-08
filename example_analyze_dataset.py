4b1d247b0a27a86f31acd74d7082e2ed42fbe5df
adamsyah <fadillahadam11@gmail.com>
2021-07-08 14:05:05 +0700

    Update function

--- example_analyze_dataset.py
+++ example_analyze_dataset.py
@@ -1,4 +1,5 @@
 from cv_utils.object_detection.dataset.analyzer import coco_dataset_analysis
+from cv_utils.object_detection.dataset.analyzer import yolo_dataset_analysis
 
 path_annotation_dictionary = {
     set_name: f"test/ki67/instances_{set_name}.json"
@@ -6,3 +7,11 @@ path_annotation_dictionary = {
 }
 
 coco_dataset_analysis(path_annotation_dictionary)
+
+yolo_label_dir = {
+    "train": "test/dataset/labels/train",
+    "val": "test/dataset/labels/val",
+    "test": "test/dataset/labels/test",
+}
+yolo_class_path = "test/dataset/classes.txt"
+yolo_dataset_analysis(yolo_class_path, yolo_label_dir)
