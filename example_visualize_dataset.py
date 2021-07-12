b92d763252ca92a6f7b63ddd400ebaaffce21e19
adamsyah <fadillahadam11@gmail.com>
2021-07-12 22:16:23 +0700

    Update for demos

--- example_visualize_dataset.py
+++ example_visualize_dataset.py
@@ -1,10 +1,17 @@
 from cv_utils.object_detection.dataset.visualizer import visualize_yolo
 from cv_utils.object_detection.dataset.visualizer import visualize_coco
    
-visualize_yolo('test/dataset/images/test/1a22388ab2a54e.jpg',
-               'test/dataset/labels/test/1a22388ab2a54e.txt',
-               'test/dataset/classes.txt', True, 'test/demo.jpg', 0.8)
+visualize_yolo(
+    image_path = 'demo/dataset/fasciola_yolo/images/test/49c17b90910b4a.jpg',
+    label_path = 'demo/dataset/fasciola_yolo/labels/test/49c17b90910b4a.txt',
+    yolo_class_file = 'demo/dataset/fasciola_yolo/classes.txt', show = True,
+    save_labeled_path = None, max_image_to_screen_ratio = 0.8,
+    seed = 10
+)
 
-visualize_coco('test/sel_telur/test/49c17b90910b4a.jpg',
-               'test/sel_telur/annotations/instances_test.json',
-               True, 'test/demo_coco.jpg', 0.8)
+visualize_coco(
+    image_path = 'demo/dataset/fasciola_ori/test/222e90ca7a0444.jpg',
+    annotation_path = 'demo/dataset/fasciola_ori/annotations/instances_test.json',
+    show = False, save_labeled_path = 'demo/dataset/sample.jpg',
+    max_image_to_screen_ratio = 0.8, seed = 42
+)
