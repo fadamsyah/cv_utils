bfa415cea603aeb6b22a71e4e673b2cf4da11cb1
adamsyah <fadillahadam11@gmail.com>
2021-11-03 12:44:07 +0700

    Update imports

--- cv_utils/WSI/__init__.py
+++ cv_utils/WSI/__init__.py
@@ -1,4 +1,5 @@
 from .convert import xml_to_mask
 from .patches import get_patches_coor, generate_training_patches
+from .patches import get_thumbnail, get_slide_crop
 from .utils import pil_to_cv2, xml_to_df
 from .viz import viz_with_xml, viz_with_mask
--- cv_utils/WSI/patches/__init__.py
+++ cv_utils/WSI/patches/__init__.py
@@ -1,2 +1,5 @@
 from .functional import get_patches_coor
 from .patches import generate_training_patches
+from .patches import process_thumbnail_binary_map
+from .utils import get_thumbnail, get_slide_crop
+from .utils import get_size
