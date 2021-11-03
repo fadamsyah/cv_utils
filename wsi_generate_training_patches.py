c7c7ba82e0136d77b571c8f453f560c7aa0f0289
adamsyah <fadillahadam11@gmail.com>
2021-11-03 17:34:47 +0700

    Add CLIs

--- wsi_generate_training_patches.py
+++ wsi_generate_training_patches.py
@@ -1,7 +1,22 @@
-from cv_utils.WSI import generate_training_patches
+import argparse
 
-path_slide = 'WSI/slide/tumor/tumor_026.tif'
-path_mask = 'WSI/mask/tumor/tumor_026.tif'
+from cv_utils import WSI
+from cv_utils import boolean_string
+
+"""
+Example:
+    - python wsi_generate_training_patches.py -w WSI/slide/tumor/tumor_019.tif \
+        -m WSI/mask/tumor/tumor_019.tif --ext png --overwrite True
+"""
+
+parser = argparse.ArgumentParser(description='Visualize Annotation')
+parser.add_argument('-w', '--wsi', type=str, default=None, help='WSI path')
+parser.add_argument('-m', '--mask', type=str, default=None, help='mask path')
+parser.add_argument('-e', '--ext', type=str, default='png', help='patches extension')
+parser.add_argument('-o', '--overwrite', type=boolean_string, default=False,
+                    help='(True or False) overwrite the save directories')
+args = parser.parse_args()
+
 level = 0
 patch_size = (300, 300)
 stride = (128, 128)
@@ -10,10 +25,9 @@ min_pct_tissue_area = 0.1
 min_pct_tumor_area = 0.05
 save_dir = {'tumor': 'WSI/patches/tumor',
             'normal': 'WSI/patches/normal'}
-EXTENSION = 'tif'
-OVERWRITE = False
 
-generate_training_patches(path_slide, path_mask, level, patch_size, stride, inspection_size,
-                          save_dir, drop_last=True, min_pct_tissue_area=min_pct_tissue_area,
-                          min_pct_tumor_area=min_pct_tumor_area, ext=EXTENSION,
-                          overwrite=OVERWRITE)
+WSI.generate_training_patches(
+    args.wsi, args.mask, level, patch_size, stride, inspection_size, save_dir, drop_last=True,
+    min_pct_tissue_area=min_pct_tissue_area, min_pct_tumor_area=min_pct_tumor_area, ext=args.ext,
+    overwrite=args.overwrite
+)
