2a708d8ecd3b087faef1a71ceaf73be9067d9097
adamsyah <fadillahadam11@gmail.com>
2021-11-09 17:09:33 +0700

    Updates

--- cv_utils/image_classification/metrics.py
+++ cv_utils/image_classification/metrics.py
@@ -1,29 +1,45 @@
 import torchmetrics
 
 class Accuracy(torchmetrics.Accuracy):
-    def __init__(self, **kwargs):
+    def __init__(self, use_cuda=False, **kwargs):
         super().__init__(**kwargs)
+        self.use_cuda = use_cuda
     
     def __call__(self, preds, target, *args):
-        return super().__call__(preds, target.int(), *args)
+        y_true = target.int()
+        if self.use_cuda:
+            y_true = y_true.cuda()
+        return super().__call__(preds, y_true, *args)
 
 class Precision(torchmetrics.Precision):
-    def __init__(self, **kwargs):
+    def __init__(self, use_cuda=False, **kwargs):
         super().__init__(**kwargs)
+        self.use_cuda = use_cuda
     
     def __call__(self, preds, target, *args):
-        return super().__call__(preds, target.int(), *args)
+        y_true = target.int()
+        if self.use_cuda:
+            y_true = y_true.cuda()
+        return super().__call__(preds, y_true, *args)
 
 class Recall(torchmetrics.Recall):
-    def __init__(self, **kwargs):
+    def __init__(self, use_cuda=False, **kwargs):
         super().__init__(**kwargs)
+        self.use_cuda = use_cuda
     
     def __call__(self, preds, target, *args):
-        return super().__call__(preds, target.int(), *args)
+        y_true = target.int()
+        if self.use_cuda:
+            y_true = y_true.cuda()
+        return super().__call__(preds, y_true, *args)
 
 class F1(torchmetrics.F1):
-    def __init__(self, **kwargs):
+    def __init__(self, use_cuda=False, **kwargs):
         super().__init__(**kwargs)
+        self.use_cuda = use_cuda
     
     def __call__(self, preds, target, *args):
-        return super().__call__(preds, target.int(), *args)
+        y_true = target.int()
+        if self.use_cuda:
+            y_true = y_true.cuda()
+        return super().__call__(preds, y_true, *args)
