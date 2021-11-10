aa87334f14b9701d8ec47fc3d307da10f53f470a
adamsyah <fadillahadam11@gmail.com>
2021-11-10 14:07:01 +0700

    Update cv_utils

--- cv_utils/image_classification/model_wrapper.py
+++ cv_utils/image_classification/model_wrapper.py
@@ -59,6 +59,22 @@ class CommonModelWrapper(pl.LightningModule):
             for name, func in self.metrics.items():
                 self.log(f"test_{name}", func(out, mask), on_step=False, on_epoch=True, prog_bar=True, logger=True)
     
+    def load_model(self, checkpoint_path, evaluation=True, requires_grad=True):
+        checkpoint = torch.load(checkpoint_path, map_location=torch.device('cpu'))
+        
+        self.model.load_state_dict(checkpoint['state_dict'])
+        
+        self.model.requires_grad_(requires_grad)
+        
+        if evaluation: self.model.eval()
+        else: self.model.train()
+    
+    def cpu(self):
+        self.model.cpu()
+    
+    def cuda(self):
+        self.model.cuda()
+    
     def set_optim_hyp(self, class_alg, hyp_dict):
         self.optim_alg = class_alg
         for key, val in hyp_dict.items():
