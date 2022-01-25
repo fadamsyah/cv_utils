ea17fe9837e3e09556305868e5c25edf25507493
ideapad300 <fadillahadam11@gmail.com>
2022-01-25 13:44:53 +0700

    Update typing

--- cv_utils/image_classification/conv2d.py
+++ cv_utils/image_classification/conv2d.py
@@ -10,6 +10,6 @@ class Conv2dSamePadding(nn.Conv2d):
         super(Conv2dSamePadding, self).__init__(*args, **kwargs)
         self.zero_pad_2d = nn.ZeroPad2d(reduce(__add__,
             [(k // 2 + (k - 2 * (k // 2)) - 1, k // 2) for k in self.kernel_size[::-1]]))
-
+    
     def forward(self, input):
         return  self._conv_forward(self.zero_pad_2d(input), self.weight, self.bias)
--- cv_utils/utils/utils.py
+++ cv_utils/utils/utils.py
@@ -3,13 +3,13 @@ import shutil
 
 from pathlib import Path
 
-def boolean_string(s):
+def boolean_string(s: str) -> bool:
     # Ngambil dari zyolo efficientdet
     if s not in {'False', 'True'}:
         raise ValueError('Not a valid boolean string')
     return s == 'True'
 
-def create_and_overwrite_dir(path_dir):
+def create_and_overwrite_dir(path_dir: str) -> None:
     # Create the directory
     Path(path_dir).mkdir(parents=True, exist_ok=True)
     
