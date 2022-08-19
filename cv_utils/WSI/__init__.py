2f2370498a0957226ddd61421d2bf43ddb08a9c9
fadamsyah <fadillahadam11@gmail.com>
2022-08-20 00:12:52 +0400

    Fix try except when importing multiresolutionimageinterface

--- cv_utils/WSI/__init__.py
+++ cv_utils/WSI/__init__.py
@@ -6,9 +6,10 @@ from .patches import (
     get_thumbnail, get_slide_crop
     )
 from .utils import pil_to_cv2, xml_to_df
-from .viz import viz_with_xml, viz_with_mask
 
-try: from .convert import xml_to_mask
+try:
+    from .convert import xml_to_mask
+    from .viz import viz_with_xml, viz_with_mask
 except:
     print("The multiresolutionimageinterface package is not installed.")
     print("Please refer to the ASAP repo if you want to install the package.")
